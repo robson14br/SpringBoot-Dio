9ccffea2217ab864cd861deceeda140e5659f636
Robson <robson14br@outlook.com>
2024-06-10 10:35:34 -0300

    Auth Database

---
+++ dio-spring-security/src/main/java/dip/dio_spring_security/model/User.java
@@ -0,0 +1,74 @@
+package dip.dio_spring_security.model;
+
+import javax.persistence.*;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tab_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Integer id;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 20, nullable = false)
+    private String username;
+    @Column(length = 100, nullable = false)
+    private String password;
+    @ElementCollection(fetch = FetchType.EAGER)
+	  @CollectionTable(name = "tab_user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role_id") 
+	  private List<String> roles = new ArrayList<>();
+
+    public User(){
+
+    }
+    public User(String username){
+        this.username = username;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public List<String> getRoles() {
+        return roles;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+    
+    
+}
